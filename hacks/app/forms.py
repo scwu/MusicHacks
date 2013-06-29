0661e37fa86f1d60f2bbd207d7fc6b19648cc53b
Nick Meyer <nicholasmeyer@gmail.com>
2013-06-29 15:33:21 -0400

    pretty button

--- hacks/app/forms.py
+++ hacks/app/forms.py
@@ -2,7 +2,4 @@ from django import forms
 from hacks.app.models import Genre
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    description = forms.CharField(max_length=255)
-    genre = forms.ModelChoiceField(queryset=Genre.objects.all(), empty_label="(Nothing)")
     file = forms.FileField()
